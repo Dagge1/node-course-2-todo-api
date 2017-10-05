a40193ae3dd1c81c89bf061312e1073341852c14
Davor Cuvalo <adriatic111@yahoo.com>
2017-10-05 19:12:31 +0200

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,10 +5,17 @@ const request = require('supertest');
 const {app} = require('./../server');  // relative path, zatim jedan dir natrag i onda fajl
 const {Todo} = require('./../models/todo');
 
+const todos = [{  // prije testa u lekciji 76 moramo dodati par unosa jer je prijašnji test ispraznio bazu
+    text: 'First test todo'     // array sa dva objekta tj dva unosa u bazu
+}, {
+    text: 'Second test todo'
+}];
+
+
 beforeEach((done) => {   // prije svakog testa provjeri dali je baza prazna prije svakog testa,
-    Todo.remove({}).then(() => {    // ako nije onda isprazni
-        done();
-    });
+    Todo.remove({}).then(() => {  // isprazni bazu, zatim..
+        Todo.insertMany(todos);   // mongoose method koji ubacuje više recorda/dokumenta odjednom (iz arraya todos)
+    }).then(() => done());    
 });
 
 describe('POST /todos', () => {
@@ -16,7 +23,7 @@ describe('POST /todos', () => {
         var text = 'Test todo text';
 
         request(app)
-        .post('./todos')
+        .post('/todos')
         .send({text})
         .expect(200)
         .expect((res) => {
@@ -27,11 +34,24 @@ describe('POST /todos', () => {
                 return done(err);
             }
             
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
             }).catch((e) => done(e));  // statement arrow function syntax    
         });
+    });
+});
+// tu fali testni kod iz prijašnje test lekcije
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+           expect(res.body.todos.length).toBe(2); 
+        })
+        .end(done);
     });
 });
