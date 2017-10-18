e4430f00622c244f300eee41f6fe9221f2b71770
Davor Cuvalo <adriatic111@yahoo.com>
2017-10-18 21:35:32 +0200

    POST into db

--- server/server-moj.js
+++ server/server-moj.js
@@ -44,7 +44,7 @@ app.post('/todos', (req, res) => { // todos je naziv lokacije u browseru, može 
 app.get('/all', (req, res) => { 
     Todo.find().then((todos) => {  // pronađi sve todo unose. Da je query find(nešto) prikazao bi filtrirano
     for (var i = 0; i < todos.length; i++) {
-        res.send(todos[i].text);    // ako ok šalji podatke natrag. todos je samo placeholder ime
+        res.send(todos[i].text);    // ispiši samo polje 'text' iz baze
     }
 }, (e) => {           // promise u slučaju da bude rejected
         res.status(400).send(e); 
