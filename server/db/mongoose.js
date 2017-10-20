8a826190d72da4f40db20c48ae8a45c93e06bda7
Davor Cuvalo <adriatic111@yahoo.com>
2017-10-20 14:54:43 +0200

    New changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,8 @@ var mongoose = require('mongoose');
 // spajanje na bazu
 mongoose.Promise = global.Promise;  // koristimo default promises umjesto callbacks
 
-mongoose.connect(process.env.MONGODB_URI);
+//mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.NODE_ENV);
 
 module.exports = {  // exportiranje podataka
     mongoose: mongoose   // može i samo = {mongoose}; jer je identično ime
